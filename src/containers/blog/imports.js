8cbb9ad201ada7c30e492e9dbe9b3e94f98e8303
jsalgado77 <100861444+jsalgado77@users.noreply.github.com>
2024-01-19 12:18:58 -0600

    Changes to image naming utilized in the blog section of the webpage + completion of the blog section and article component

---
+++ src/containers/blog/imports.js
@@ -0,0 +1,13 @@
+import blog01 from '../../assets/DataTunnel.png';
+import blog02 from '../../assets/DataSil.png';
+import blog03 from '../../assets/DataChain.png';
+import blog04 from '../../assets/BinaryWall.png';
+import blog05 from '../../assets/BinaryExclam.png';
+
+export {
+  blog01,
+  blog02,
+  blog03,
+  blog04,
+  blog05,
+};
